17fc42af8af67e31bb4b5cefa259e41b27b5fff2
oardilac <odardilacueto@gmail.com>
2025-02-26 16:42:24 -0500

    update metadata for electricaribe site, including URLs, application name, and descriptions

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -21,15 +21,15 @@ export function constructMetadata({
   noIndex?: boolean;
 } = {}): Metadata {
   return {
-    metadataBase: new URL("https://www.tumaquinaya.com/"), // Base URL for metadata
+    metadataBase: new URL("https://www.electricaribe.me/"), // Base URL for metadata
     title,
     description,
     openGraph: {
       title,
       description,
       type: "website", // Open Graph type
-      url: "https://www.tumaquinaya.com", // Canonical URL
-      siteName: "TuMaquinaYa", // Site name for Open Graph
+      url: "https://www.electricaribe.me", // Canonical URL
+      siteName: "electricaribe", // Site name for Open Graph
       images: [
         {
           url: image, // Image URL for Open Graph
@@ -45,14 +45,14 @@ export function constructMetadata({
       creator: "@oardilac", // Creator's Twitter handle
     },
     icons,
-    applicationName: "TuMaquinaYa", // Name of the application
-    authors: [{ name: "Oliver Ardila", url: "https://www.tumaquinaya.com" }], // Author information
+    applicationName: "electricaribe", // Name of the application
+    authors: [{ name: "Oliver Ardila", url: "https://www.electricaribe.me" }], // Author information
     generator: "Next.js", // Generator used
     referrer: "origin", // Referrer policy
     creator: "Oliver Ardila", // Creator's name
-    publisher: "TuMaquinaYa", // Publisher information
+    publisher: "electricaribe", // Publisher information
     robots: noIndex ? "noindex, nofollow" : "index, follow", // Robots meta tag based on noIndex flag
-    manifest: "https://www.tumaquinaya.com/manifest.json", // Web app manifest URL
+    manifest: "https://www.electricaribe.me/manifest.json", // Web app manifest URL
     verification: {
       google: "nu89Oy6CNw8drpQRvc3jPobUcxt7xd4wkjmIr7_T1bE", // Google site verification code
       yandex: "2f2bd53bfa136f72", // Yandex site verification code
@@ -60,18 +60,18 @@ export function constructMetadata({
     appleWebApp: {
       capable: true, // Indicates if the site is capable of running as a web app on Apple devices
       title:
-        "Alquiler de Maquinaria Amarilla y Pesada en Colombia | TuMaquinaYa", // Title for Apple Web App
+        "Alquiler de Maquinaria Amarilla y Pesada en Colombia | electricaribe", // Title for Apple Web App
       statusBarStyle: "black-translucent", // Status bar style for Apple Web App
     },
     formatDetection: { telephone: false }, // Disable automatic phone number detection
     abstract:
-      "Conecta contratistas y proveedores de maquinaria pesada en Colombia como retroexcavadoras. Alquila equipos para construcción fácilmente y de forma segura.", // Abstract of the page content
-    category: "Marketplace B2B, Alquiler Maquinaria Amarilla, Construcción", // Categories for the page
+      "Ilumincacion", // Abstract of the page content
+    category: "Producto", // Categories for the page
     classification:
-      "E-commerce, Servicios B2B, Alquiler de Maquinaria, Construcción", // Classification for the page
+      "E-commerce, Servicios B2C", // Classification for the page
     other: {
       customMeta:
-        "Alquiler seguro y eficiente de maquinaria pesada como excavadoras y bulldozers en Colombia.", // Custom metadata
+        "Iluminacion.", // Custom metadata
     },
   };
 }
